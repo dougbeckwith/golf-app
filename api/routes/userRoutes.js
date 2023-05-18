b8246f5037199f3f6edb795796b116954536f397
Doug Beckwith <dougcbeckwith@gmail.com>
2023-05-18 14:59:02 -0700

    add auth for get user

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const { createUser, getUser } = require("../controllers/userController");
+const { authenticateUser } = require("../middleware/user-auth");
 
 // Routes
-router.get("/:id", getUser);
+router.get("/", authenticateUser, getUser);
 router.post("/", createUser);
 
 module.exports = router;
