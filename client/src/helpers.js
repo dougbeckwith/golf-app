0a3a0d551de4af1c243d3be1a63cc65c31eb1142
Doug Beckwith <dougcbeckwith@gmail.com>
2023-06-09 13:37:42 -0700

    update helper functions

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from "uuid";
+
 // shotType = 'carryDistance' | 'totalDistance'
 const sortClubsByDistance = (clubs, shotType) => {
   const clubsWithAverageYards = [];
@@ -50,9 +52,60 @@ const getDistanceBarPercentage = (averageDistance, longestDistance) => {
   } else return percentage.slice(2);
 };
 
+const getAveragePutsPerRound = (rounds) => {
+  let totalPuts = 0;
+  const totalRounds = rounds.length;
+
+  rounds.forEach((round) => {
+    totalPuts += round.puts;
+  });
+
+  return totalPuts / totalRounds;
+};
+
+// temp data for putting chart
+// will remove later
+const getDummyDataPuts = () => {
+  const start = new Date("01/01/2023");
+  const end = new Date("01/10/2023");
+
+  let loop = new Date(start);
+
+  const dates = [];
+
+  function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  while (loop <= end) {
+    let date = loop.toLocaleString().slice(0, 9);
+    if (date[8] === ",") {
+      date = date.slice(0, 8);
+    }
+    dates.push(date);
+    let newDate = loop.setDate(loop.getDate() + 1);
+    loop = new Date(newDate);
+  }
+
+  const createPutRounds = () => {
+    const rounds = [];
+    for (let i = 0; i < 10; i++) {
+      const rndInt = randomIntFromInterval(27, 45);
+      rounds.push({
+        puts: rndInt,
+        date: dates[i],
+        roundId: uuidv4()
+      });
+    }
+    return rounds;
+  };
+  return createPutRounds();
+};
 export {
   getAverageDistance,
   getDistanceBarPercentage,
   sortClubsByDistance,
-  findClubById
+  findClubById,
+  getDummyDataPuts,
+  getAveragePutsPerRound
 };
